7e545b4fa19c278cd7570c36bbd2c268a98a5a2a
Robert Wiley <ylee@bodhilinux.com>
2020-07-22 09:36:34 -0400

    add src

--- src/json-array00.c
+++ src/json-array00.c
@@ -1,52 +1,16 @@
 // gcc json-array00.c -ljson-c -o json-array00
-// https://blog.codinghorror.com/sorting-for-humans-natural-sort-order/
+
 #include <stdio.h>
 #include <json-c/json.h>
-#include <string.h>
-
-#define NUM_KEYS 2
-
-static int
-sort_fn(const void *j1, const void *j2)
-{
-   json_object *const *jso1, *const *jso2;
-   const char *str1, *str2;
-
-   jso1 = (json_object *const *) j1;
-   jso2 = (json_object *const *) j2;
-
-   if (!*jso1 && !*jso2)
-       return 0;
-   if (!*jso1)
-       return -1;
-   if (!*jso2)
-       return 1;
-
-   str1 = json_object_get_string(*jso1); // This will always work even if not string
-   str2 = json_object_get_string(*jso2);
-
-   return strcmp(str1, str2);
-}
 
 int
 main(void)
 {
-   json_object *str, *temp;
-   char *keys[NUM_KEYS] = {"Vladimir", "Nathaniel"};
 
-   json_object *root = json_object_from_file("names.json");
-   printf("The json representation:\n\n%s\n\n", json_object_to_json_string_ext(root, JSON_C_TO_STRING_PRETTY));
-
-   json_object_array_sort(root, sort_fn);
-   printf("The json representation:\n\n%s\n\n", json_object_to_json_string_ext(root, JSON_C_TO_STRING_PRETTY));
-
-   for(int i=0; i <NUM_KEYS; i++)
-   {
-     str = json_object_new_string(keys[i]);
-     temp = json_object_array_bsearch(str, root, sort_fn);
-     printf("Search for key '%s' yields: %s\n", keys[i], json_object_get_string(temp));
-     json_object_put(str);
-   }
+   json_object *root = json_tokener_parse("[\"foo\", \"bar\", 32, null]");
+   printf("The json string:\n\n%s\n\n", json_object_to_json_string(root));
+   
+   printf("The json object to string:\n\n%s\n", json_object_to_json_string_ext(root, JSON_C_TO_STRING_PRETTY));
 
    json_object_put(root);
 
