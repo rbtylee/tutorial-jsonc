4fc720685e53165d04eb58ec1c3ed1f275229375
Robert Wiley <ylee@bodhilinux.com>
2020-07-22 13:25:37 -0400

    Update json-parse06.c

--- src/json-parse06.c
+++ src/json-parse06.c
@@ -9,7 +9,7 @@ main(void)
 {
    json_object *root = json_object_from_file("contact.json");
    json_object_object_foreach(root, key, val)
-   printf("%s  -> %s\n", key, json_object_get_string(val));
+      printf("%s  -> %s\n", key, json_object_get_string(val));
 
    json_object_put(root);
    return 0;
